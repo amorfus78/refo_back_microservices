dd9a58d49b28045227ef153be82d14860adbff44
Etienne Dubois <etienne.dubois@qonto.com>
2024-12-18 13:55:13 +0100

    add gateway

--- student/src/main/java/com/renfobackend/student/http/SchoolService.java
+++ student/src/main/java/com/renfobackend/student/http/SchoolService.java
@@ -1,7 +1,12 @@
 package com.renfobackend.student.http;
 
-import org.springframework.web.client.RestTemplate;
+import java.util.List;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerInterceptor;
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.web.client.RestTemplate;
 import com.netflix.appinfo.InstanceInfo;
 import com.renfobackend.student.dtos.SchoolDto;
 import com.netflix.discovery.EurekaClient;
@@ -9,15 +14,19 @@ import com.netflix.discovery.EurekaClient;
 
 
 public class SchoolService {
+	@LoadBalanced
 	final private RestTemplate restTemplate = new RestTemplate();
 
 	public SchoolDto getSchoolDetails(Long schoolId, EurekaClient discoveryClient) {
 		InstanceInfo service = discoveryClient
-      .getApplication("school")
+      .getApplication("SCHOOL")
       .getInstances()
 			.get(0);
 			
-		int port = service.getPort();
+		// Using Eureka as discovery client, and knowing the RestTemplate is load balanced,
+		// we can directly use the instance info to get the school details
+
+		int port = service.getPort();		
 		String host = service.getHostName();
 
 		String url = "http://" + host + ":" + port + "/schools/" + schoolId;
