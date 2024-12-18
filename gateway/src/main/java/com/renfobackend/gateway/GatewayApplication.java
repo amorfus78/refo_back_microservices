ed558634d4c279bd8ad077f242d9d04c494d63b3
Etienne Dubois <etienne.dubois@qonto.com>
2024-12-18 13:56:41 +0100

    really add gateway

---
+++ gateway/src/main/java/com/renfobackend/gateway/GatewayApplication.java
@@ -0,0 +1,38 @@
+package com.renfobackend.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class GatewayApplication {
+
+	@Bean
+	public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+		return builder.routes()
+			.route("students", r -> r
+				.path("/student/**")
+				.filters(f -> {
+					return f.stripPrefix(1);
+				})
+				.uri("lb://STUDENT")
+			)
+			.route("school", r -> r
+				.path("/schools/**")
+				.filters(f -> {
+					return f.stripPrefix(1);
+				})
+				.uri("lb://SCHOOL")
+			)
+			.build();
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+}
